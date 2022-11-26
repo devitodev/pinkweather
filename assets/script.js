454e1c7aa78208904d9ceb298bfc90b4af2f5bd9
devitodev <softwaredev.ito@gmail.com>
2022-11-26 13:56:30 -0600

    Setting up JS with getting IDs using Jquery

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const time = $('#time');
+const date = $('#date');
+const currentWeatherItems = $('#current-weather-items');
+const timeZone = ('#time-zone');
+const country = $('country');
+const weatherForecast = $('weather-forecast');
+const currentTemp = $('current-temp');
+
