b4de76a3e0c7a574f5c4792faff5548be303d0b3
devitodev <softwaredev.ito@gmail.com>
2022-11-27 20:38:15 -0600

    set up API call in JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Retrieving Important Elements
 const dateArea = $('.time-date');
 const currentWeatherItems = $('#current-weather-items');
 const timeZone = ('#time-zone');
@@ -5,28 +6,34 @@ const country = $('country');
 const weatherForecast = $('weather-forecast');
 const currentTemp = $('current-temp');
 
-var currentDate = dayjs().format('MMM D , YYYY')
-
-
+// Setting and displaying date and time
+var currentDate = dayjs().format('MMM D , YYYY');
 function myTimer() {
-    const newDay = new Date()
-    dateArea.text(currentDate + ' ' + newDay.toLocaleTimeString())
-  }
+    const newDay = new Date();
+    dateArea.text(currentDate + ' ' + newDay.toLocaleTimeString());
+  };
 
-  myTimer()
+//   Calling timer function
+  myTimer();
 
-  var timeInterval = setInterval(myTimer, 1000)
+// Allowing for timer to update live
+  var timeInterval = setInterval(myTimer, 1000);
 
-// To Do: Switch to all Day JS
-// setInterval(() => {
-    // const clockTime = new Date();
-    // const month = dayjs().month();
-    // const date = time.getDate();
-    // const day = time.getDay();
-    // const hour = time.getHours();
-    // const hoursIn12 = hour >= 13 ? hour %12: hour;
-    // const minutes = time.getMinutes();
-    // const ampm = hour >=12 ? 'PM' : 'AM';
 
-//     time.innerHTML = dayjs()
-// }, 1000);
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const months = ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const weatherAPIKey = 'd38245ae19141a85c5b74ef46109d23c';
+
+pullWeatherData();
+function pullWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude} = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${weatherAPIKey}`).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    })
+} 
